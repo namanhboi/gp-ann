5d89c82ef4b94565d8589a36df5fb9f59b755988
Nam Anh <dangnamanh05@gmail.com>
2025-10-28 13:47:13 -0400

    Exposed the ReadBytes function to use

--- src/points_io.h
+++ src/points_io.h
@@ -9,3 +9,8 @@ void WritePoints(PointSet& points, const std::string& path);
 std::vector<NNVec> ReadGroundTruth(const std::string& path);
 
 void WriteGroundTruth(const std::string& path, const std::vector<NNVec>& ground_truth);
+
+namespace internal {
+  template <typename CoordinateType> PointSet ReadBytes(const std::string &path);
+  PointSet ReadPoints(const std::string &path, int64_t size);
+}
